c69010d0a823286091d75ebb62b81a8f7f1c36aa
Jacob Haslehurst <jacob@haslehurst.net>
2014-06-27 10:44:12 +1000

    bump to 0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-currency-exchange',
-    version='0.1.1',
+    version='0.1.2',
     description='Django Foreign Exchange',
     author='Gizmag',
     author_email='tech@gizmag.com',
